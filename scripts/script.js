cb8f3457ee50f0c5dec2b7b67a9516053fc2d621
Elias <jelias@NATL2236LJELIAS.local>
2018-11-06 13:47:00 -0500

    displays all districts for each county

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,13 @@
 
 var data_url = './data/votes_data.csv' //url of data file
-var data = {"counties":[]}
-
+//var data = {"counties":[]}
+var result = {}
+var headers = [
+    {"label":"District", "value":"ajc_precinct"},
+    {"label":"Democratic", "value":"dem_votes"},
+    {"label":"Republican", "value":"rep_votes"},
+    {"label":"Average Income", "value":"avg_income"}
+]
 /*
 data = {
     counties: [
@@ -15,17 +21,18 @@ data = {
 }
 */
 
-//On load opens csv
-$(document).ready(function() {
-    $.ajax({
-        type: "GET",
-        url: data_url,
-        dataType: "text",
-        success: function(stuff) { data = processData(stuff);}
-     });
-     console.log(data)
-});
 
+var updateSelect = function(data) {
+    var select = document.getElementById('inputCounty')
+    console.log(this.data)
+    for (var county in data["counties"]) {
+        console.log(county)
+        var opt = document.createElement('option');
+        opt.value = data.counties[county].fips;
+        opt.innerHTML = data.counties[county].name;
+        select.appendChild(opt)
+    }
+}
 //Calls functions to take data from csv to an object
 var processData = function(data) {
     return csvToArray(data)
@@ -48,11 +55,12 @@ var csvToArray = function(allText) {
             lines.push(tarr);
         }
     }    
-    arrayToObject(headers, lines)
+    return arrayToObject(headers, lines)
 }
 
 //Processes from this array to an object
 var arrayToObject = function(headers, input) {
+    var data = {"counties":[]} 
     for (var item in input) {
         var temp = {}
         var county = {}
@@ -65,9 +73,9 @@ var arrayToObject = function(headers, input) {
             }
         }
 
-        var index = countyExist(data.counties, county['name'])
+        var index = countyExist(data.counties, county['fips'])
         if (index != -1) {
-            data.counties[index].precincts.push(temp)
+            data.counties[index]['precincts'].push(temp)
         } else {
             county['precincts'] = [] 
             county['precincts'].push(temp)
@@ -75,6 +83,7 @@ var arrayToObject = function(headers, input) {
         }
 
     }
+    return data
 
 }
 
@@ -82,13 +91,90 @@ var countyExist = function(obj, county) {
     var result
     if (obj) {
         result = obj.findIndex(function(element) {
-            return element.name == county
+            return element.fips == county
         })
     }
     return result
 }
 
-//Adds listener for county select button
-document.getElementById("county-select").addEventListener("submit", function() {
-    console.log("clicked")
+var getResult = function(num) {
+    var index = countyExist(result.counties,num)
+    var county;
+    if (index) {
+        county = result.counties[index]
+        console.log("hi" + index)
+        console.log("You are viewing the results for " + toProperCase(county.name))
+        $(".county-title").html("You are viewing the results for " + toProperCase(county.name) + " County.")
+        if (county.precincts.length > 0) {
+            console.log("there are results")
+            $(".precincts").append("<table><thead>")
+            for (var h in headers) {
+                $(".precincts").append("<td>" + headers[h].label + "</td>")
+            }
+            $(".precincts").append("</thead><tbody>")
+            var body = "<tbody>"
+            for (var l in county.precincts) {
+                body = body + "<tr>"
+                for (var h in headers) {
+                    body = body + "<td>"
+                    body = body + county.precincts[l][headers[h].value]
+                    body = body + "</td>"
+                }
+                body = body + "</tr>"
+            }
+            body = body + "</tbody></table>"
+            $(".precincts").append(body)
+        } else {
+            $(".county-title").html("Result for this " + county.name +" are not available yet")
+        }
+    } else {
+        $(".county-title").html("Result for this county cannot be found")
+    }
+    console.log(county)
+    //$("#county-title").innerHTML = ""
+}
+
+var getCounty = function(fips) {
+    for (var m in result) {
+        if (result.counties[m].fips = fips) {
+            return result.counties[m]
+        } 
+    }
+}
+
+var toProperCase = function(str) {
+    return str.replace(/\w\S*/g, function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+
+//On load opens csv
+$(document).ready(function() {
+    $(".county-title").html("")
+    $(".precincts").html("")
+    $.ajax({
+        beforeSend: function() {
+            document.body.style.cursor='wait';
+        },
+        type: "GET",
+        url: data_url,
+        dataType: "text",
+        success: function(stuff) { 
+            data = processData(stuff); 
+            updateSelect(data)
+            result = data
+            document.body.style.cursor='default';
+        }
+    });
+    $("#county-select").submit(function(e) {
+        e.preventDefault()
+        $(".county-title").html("")
+        $(".precincts").html("")
+        $( "#county-select option:selected" ).val();
+        var value = $("#county-select option:selected").val();
+        getResult(value)
+        console.log("FIPS " + value)
+    })
+
 });
+//Adds listener for county select button
