84cbe41bf88a5aafb4f437c20bcf857c854e51b3
Elias <jelias@NATL2236LJELIAS.local>
2018-11-06 10:47:14 -0500

    working from csv to object

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# election-results-by-district
+# Live Election Results by District
+This app takes a data file of the results of voting precincts and displays them by county. 
+
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,16 @@
-var data_url = './data/votes_data.csv'
-var data = {}
+
+var data_url = './data/votes_data.csv' //url of data file
+var data = {"counties":[]}
 
 /*
 data = {
     counties: [
         {
-            county: 'Fulton'
+            county: 'Fulton',
+            fips: #,
             precincts: [{}]
         }
     ]
-]
-    county: 
-    precincts{
-
-    }
 ]
 }
 */
@@ -24,13 +21,17 @@ $(document).ready(function() {
         type: "GET",
         url: data_url,
         dataType: "text",
-        success: function(data) { data = processData(data);}
+        success: function(stuff) { data = processData(stuff);}
      });
+     console.log(data)
 });
 
+//Calls functions to take data from csv to an object
 var processData = function(data) {
     return csvToArray(data)
 }
+
+//Processes from the csv to an array of headers and items
 var csvToArray = function(allText) {
     var allTextLines = allText.split(/\r\n|\n/);
     var headers = allTextLines[0].split(',');
@@ -47,15 +48,47 @@ var csvToArray = function(allText) {
             lines.push(tarr);
         }
     }    
-    console.log(headers)
-    console.log(lines)
     arrayToObject(headers, lines)
 }
 
+//Processes from this array to an object
 var arrayToObject = function(headers, input) {
+    for (var item in input) {
+        var temp = {}
+        var county = {}
+        county['name'] = input[item][10]
+        county['fips'] = input[item][11]
+        for (var m in headers) {
+            var ignore = [9,10,11,15,19]
+            if (!ignore.includes(parseInt(m))) {
+                temp[headers[m]] = input[item][m]
+            }
+        }
+
+        var index = countyExist(data.counties, county['name'])
+        if (index != -1) {
+            data.counties[index].precincts.push(temp)
+        } else {
+            county['precincts'] = [] 
+            county['precincts'].push(temp)
+            data.counties.push(county)
+        }
+
+    }
 
 }
 
+var countyExist = function(obj, county) {
+    var result
+    if (obj) {
+        result = obj.findIndex(function(element) {
+            return element.name == county
+        })
+    }
+    return result
+}
+
+//Adds listener for county select button
 document.getElementById("county-select").addEventListener("submit", function() {
     console.log("clicked")
 });
